3dfe67b1357bdea8546e189678c095b1e27cdb2e
jaywcjlove <398188662@qq.com>
2023-07-23 02:03:29 +0800

    fix: fix CodeProps props.

--- core/src/Code.tsx
+++ core/src/Code.tsx
@@ -1,22 +1,28 @@
 import React, { useContext } from 'react';
 import { Context } from './store';
 import { CODE_PREVIEW_PREFIX } from './CodePreview';
-import type { ElementType, ComponentPropsWithoutRef } from 'react';
+
+export type TagType = React.ComponentType | keyof JSX.IntrinsicElements;
+
+export interface CodeProps<Tag extends TagType> extends React.HTMLProps<Tag> {
+  tagName?: Tag;
+}
 
 export const Code = React.forwardRef(
-  <T extends ElementType<any> = 'div'>(props: { as?: T } & ComponentPropsWithoutRef<T>, ref?: React.LegacyRef<T>) => {
-    const { as, className, children, ...htmlProps } = props;
+  <Tag extends TagType = 'div'>(props: CodeProps<Tag>, ref: React.Ref<React.HTMLProps<Tag>>) => {
+    const { tagName = 'div', className, children, ...htmlProps } = props;
     const cls = [`${CODE_PREVIEW_PREFIX}-code`, className].filter(Boolean).join(' ').trim();
     const store = useContext(Context);
     if (store.collapse) {
       return null;
     }
-    const Comp = as || 'div';
-    return (
-      <Comp {...htmlProps} className={cls} ref={(node) => {}}>
-        {children}
-      </Comp>
-    );
+    const TagName = props.href && typeof tagName === 'string' ? 'a' : tagName;
+    const childProps = {
+      ...htmlProps,
+      className: cls,
+      ref,
+    };
+    return React.createElement(TagName, childProps, children);
   },
 );
 
